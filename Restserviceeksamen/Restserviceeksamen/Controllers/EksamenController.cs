748a47e98f755e23730138b2d45df8755fab5260
smorebrod <36259392+smorebrod@users.noreply.github.com>
2019-09-27 13:42:28 +0200

    rest service eksamen

---
+++ Restserviceeksamen/Restserviceeksamen/Controllers/EksamenController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restserviceeksamen.model;
+
+namespace Restserviceeksamen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EksamenController : ControllerBase
+    {
+        private static List<Parkering> cList = new List<Parkering>
+        {
+            new Parkering(1,"AR12345",5,5,25,"01.05.2018"),
+            new Parkering(2,"BR45612",2,10,20,"01-05-2019"),
+            new Parkering(3,"DG26482",8,0,0,"3-05-2019"),
+            new Parkering(4,"BR45612",4,5,20,"3-05-2019"),
+            new Parkering(5,"BR45612",5,5,25,"4-05-2019")
+        };
+
+
+
+
+        // GET: api/Parkering
+        [HttpGet]
+        public List<Parkering> get()
+        {
+            return cList;
+        }
+
+        // GET: api/Parkering/5
+        [HttpGet("{id}", Name = "Get")]
+        public Parkering GetParkering(int id)
+        {
+            Parkering c = cList.FirstOrDefault(Parkering => Parkering.ID == id);
+            
+            return c;
+        }
+
+        // POST: api/Eksamen
+        [HttpPost]
+        public Parkering InsertParkering([FromBody] Parkering pakering)
+        {
+            cList.Add(pakering);
+            return pakering;
+
+        }
+
+        // PUT: api/Eksamen/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Restserviceeksamen/Restserviceeksamen/model/Parkering.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restserviceeksamen.model
+{
+    public class Parkering
+    {
+        public int ID { get; set; }
+        public string NrPlade { get; set; }
+
+        public int AntalTimer { get; set; }
+
+        public int TimePris { get; set; }
+
+        public int TotalPris { get; set; }
+
+        public string Dato { get; set; }
+
+        public Parkering()
+        {
+        }
+
+        public Parkering(int id,string nrplade, int antaltimer, int timepris, int totalpris, string dato)
+        {
+            ID = id;
+            NrPlade = nrplade;
+            AntalTimer = antaltimer;
+            TimePris = timepris;
+            TotalPris = totalpris;
+            Dato = dato;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ID)}: {ID}, {nameof(NrPlade)}: {NrPlade}, {nameof(AntalTimer)}: {AntalTimer}, {nameof(TimePris)}: {TimePris}," +
+                   $"{nameof(TotalPris)}: {TotalPris},{nameof(Dato)}: {Dato},";}
+
+        }
+    }
+
